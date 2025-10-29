c6f41bacdccc14ba4300ba4e6ccafbeee0c57c74
Victor Torres <victor.torrest@ciencias.unam.mx>
2025-10-29 13:57:07 -0600

    First commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hello from a kube")
+	})
+	fmt.Println("Server running port 8080")
+	http.ListenAndServe(":8080", nil)
+}
